e769c08485d60aa38d7f3954f137f910f673f459
Qchmqs <achmas@live.fr>
2021-09-24 22:00:35 +0100

    formatting

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-function newElement() 
+function newElement() {
+  // this code doesn't work, but it gives you an idea
+  const list = document.getElementById("myUL")
+  const newEntry = "<li></li>"
+  list.appendChild(newEntry)
+}
