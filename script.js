35ade43c597af37be8ee15d14e35cf0a7f3a8cdc
Abouaker Riche <simou.islam12@gmail.com>
2021-10-06 22:51:14 +0100

    First commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function newElement() {
   // this code doesn't work, but it gives you an idea
-  const list = document.getElementById("myUL")
-  const newEntry = "<li></li>"
-  list.appendChild(newEntry)
+  var li = document.createElement("li")
+  var newEntry = document.getElementById( "inp").value
+  var u = document.createTextNode(newEntry)
+  li.appendChild(u)
+document.getElementById("myUL").appendChild(li)
+document.getElementById("inp").value = "2";
 }
+
